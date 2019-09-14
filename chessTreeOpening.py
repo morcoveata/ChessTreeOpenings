ecd8e71d451f484074812300bf9ec2f98fca8577
Luca Maciuca <florisg2002@yahoo.com>
2019-09-14 12:20:51 +0300

    get main variation

--- chessTreeOpening.py
+++ chessTreeOpening.py
@@ -1,3 +1,4 @@
+import re
 import sys
 from PyQt4 import QtGui
 from PyQt4 import QtCore
@@ -8,8 +9,25 @@ class Window(QtGui.QMainWindow):
 	def __init__(self):
 		super(Window, self).__init__()
 
+##############################################
+		# manipulate a png string
+		# inputPng = "1. e4 e5 2. Nf3 Nc6 3. Bb5 Nf6 4.a1 (4. O-O Bc5 (4... Nxe4 5. d4)) ...Ng2"
+		# inputPng = QtGui.QPlainTextEdit(self)
+		inputPng = open('text.pgn').read()
+		# inputPng.setPlainText(text)
+		# print inputPng
 
 
+		variation = inputPng.split("(")
+		# numberVariations = len(variation)
+		
+		
+		mainVariation = re.sub(r'\(.*\)', '', inputPng)
+		print mainVariation
+
+
+		##################
+		# draw GUI
 		widget = QtGui.QWidget(self)
 		self.setCentralWidget(widget)
 
@@ -24,13 +42,15 @@ class Window(QtGui.QMainWindow):
 
 		widget.setLayout(layout)
 
+		#################################
+
 		model = QtGui.QStandardItemModel()
 		model.setHorizontalHeaderLabels(['col1', 'col2', 'col3'])
 		self.treeWiew.setModel(model)
 		self.treeWiew.setUniformRowHeights(True)
 		parent1 = QtGui.QStandardItem('mama')
 
-		child1 = QtGui.QStandardItem('Child1')
+		child1 = QtGui.QStandardItem(variation[0])
 		child2 = QtGui.QStandardItem('Child2')
 		child3 = QtGui.QStandardItem('Child3')
 		parent1.appendRow([child1, child2, child3])
@@ -41,7 +61,7 @@ class Window(QtGui.QMainWindow):
 
 
 
-
+		#set dinamicaly geometry
 		# https://programtalk.com/python-examples/PyQt4.QtGui.QDesktopWidget.screenGeometry/
 		rect = QtGui.QDesktopWidget().screenGeometry()
 		width = rect.width()
