068b8f2026fde445356630c9f04c888cf1c7be76
Luca Maciuca <florisg2002@yahoo.com>
2019-09-21 14:12:47 +0300

    get main variation and work on tree

--- chessTreeOpening.py
+++ chessTreeOpening.py
@@ -9,21 +9,98 @@ class Window(QtGui.QMainWindow):
 	def __init__(self):
 		super(Window, self).__init__()
 
+
 ##############################################
 		# manipulate a png string
-		# inputPng = "1. e4 e5 2. Nf3 Nc6 3. Bb5 Nf6 4.a1 (4. O-O Bc5 (4... Nxe4 5. d4)) ...Ng2"
-		# inputPng = QtGui.QPlainTextEdit(self)
+		# https://stackoverflow.com/questions/14596884/remove-text-between-and-in-python
+
+		# def a(test_str):
+		# 	ret = ''
+		# 	skip1c = 0
+		# 	skip2c = 0
+		# 	for i in test_str:
+		# 		if i == '[':
+		# 			skip1c += 1
+		# 		elif i == '(':
+		# 			skip2c += 1
+		# 		elif i == ']' and skip1c > 0:
+		# 			skip1c -= 1
+		# 		elif i == ')'and skip2c > 0:
+		# 			skip2c -= 1
+		# 		elif skip1c == 0 and skip2c == 0:
+		# 			ret += i
+		# 	return ret
+
+
+	
+		###############################
+
+	# function for get main variation
+
+		def a(test_str):
+			ret = ''
+			skip2c = 0
+			for i in test_str:
+				if i == '(':
+					skip2c += 1
+				elif i == ')'and skip2c > 0:
+					skip2c -= 1
+				elif skip2c == 0:
+					ret += i
+			return ret
+
+	
 		inputPng = open('text.pgn').read()
-		# inputPng.setPlainText(text)
-		# print inputPng
+
+		 
+
+		# remove head of pgn with rectangel brackets
+		finalString = re.sub(r'\[.*?\]', '', inputPng)
 
 
-		variation = inputPng.split("(")
-		# numberVariations = len(variation)
 		
+		# print finalString
+
+
 		
-		mainVariation = re.sub(r'\(.*\)', '', inputPng)
-		print mainVariation
+		finalString = a(finalString)
+		#transform pargraph sign to space sign
+		finalString = finalString.replace("\n", "")
+		# remove space sign
+		finalString = finalString.replace("  ", "")
+
+		# print finalString.strip()
+		finalString.strip()
+
+		# with open("guru99.pgn", "a") as myfile:
+		# 	myfile.write("First Line\n")
+		#     myfile.close()
+
+		###################################
+		# write header for pgn
+		with open("guru99.pgn", 'r+') as file:
+			originalContent = file.read()
+			file.seek(0, 0)              # Move the cursor to top line
+			file.write('[Event "?"]\n')
+			file.write('[Site "?"]\n')
+			file.write('[Date "????.??.??"]\n')
+			file.write('[Round "?"]\n')
+			file.write('[White "?"]\n')
+			file.write('[Black "?"]\n')
+			file.write('[Result "*"]\n')
+			file.write('[ECO "C65"]\n')
+			file.write('[Annotator "LucaCiubota_Gamer"]\n')
+			file.write('[PlyCount "31"]\n')
+			file.write('[SourceDate "2019.05.25"]\n')
+			file.write(originalContent)
+				
+
+
+		# test new pgn
+		# f= open("guru99.pgn","w+")
+		# f.write(finalString)
+		# f.close()
+
 
 
 		##################
@@ -42,23 +119,29 @@ class Window(QtGui.QMainWindow):
 
 		widget.setLayout(layout)
 
-		#################################
+	##############
+	# work on tree view
 
 		model = QtGui.QStandardItemModel()
-		model.setHorizontalHeaderLabels(['col1', 'col2', 'col3'])
+		model.setHorizontalHeaderLabels(['var 1', 'var 2', 'var 3'])
 		self.treeWiew.setModel(model)
 		self.treeWiew.setUniformRowHeights(True)
-		parent1 = QtGui.QStandardItem('mama')
 
-		child1 = QtGui.QStandardItem(variation[0])
-		child2 = QtGui.QStandardItem('Child2')
-		child3 = QtGui.QStandardItem('Child3')
-		parent1.appendRow([child1, child2, child3])
+		parent1 = QtGui.QStandardItem(finalString)
+		child1 = QtGui.QStandardItem("varianta 1")
+		child2 = QtGui.QStandardItem("varianta 2")
+		child3 = QtGui.QStandardItem("varianta 3")
+		
+		model.setItem(0,0, parent1)
+		parent1.setRowCount(0)
+		parent1.setColumnCount(0);
+		parent1.setChild(0, 0, child1);
+		parent1.setChild(1, 1, child2);
+		parent1.setChild(2, 2, child3);	
 
-		model.appendRow(parent1)
+		# parent1.appendColumn([child1, child2, child3])
 
-
-
+		# model.appendRow(parent1)
 
 
 		#set dinamicaly geometry
@@ -71,13 +154,9 @@ class Window(QtGui.QMainWindow):
 
 
 
-
-
-
-
 if __name__ == '__main__':
 	app = QtGui.QApplication([])
 	gui = Window()
 	gui.show()
 	app.exec_()
-		
+
