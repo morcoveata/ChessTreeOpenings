dd112b0222f4c19462adbb4a0e09001259c00609
Luca Maciuca <florisg2002@yahoo.com>
2019-10-10 15:04:19 +0300

    identify algorithm for all variations

--- chessTreeOpening.py
+++ chessTreeOpening.py
@@ -2,6 +2,9 @@ import re
 import sys
 from PyQt4 import QtGui
 from PyQt4 import QtCore
+from ChessBoard import ChessBoard
+from ChessClient import ChessClient
+
 
 
 class Window(QtGui.QMainWindow):
@@ -9,7 +12,22 @@ class Window(QtGui.QMainWindow):
 	def __init__(self):
 		super(Window, self).__init__()
 
+		
+		
+		#get chessboard
+		chessboard = ChessBoard()
+		chessboard.printBoard()
 
+
+
+		# chessclient = ChessClient()
+		# chessclient.mainLoop()
+
+		
+		
+		
+       
+		
 ##############################################
 		# manipulate a png string
 		# https://stackoverflow.com/questions/14596884/remove-text-between-and-in-python
@@ -49,36 +67,90 @@ class Window(QtGui.QMainWindow):
 					ret += i
 			return ret
 
+##############################################
+##############################################
+##############################################
+##############################################
+		def maxDepth(S):
+			current_max = 0
+			max = 0
+			n = len(S)
 
-					###############################
+			# Traverse the input string
+			for i in xrange(n):
+				if S[i] == '(':
+					current_max += 1
 
-	# function for get first variation 
-	# primeste textul din prima paranteza sau a doua sau a treia incastrate una in alta
+					if current_max > max:
+						max = current_max
+				elif S[i] == ')':
+					if current_max > 0:
+						current_max -= 1
+					else:
+						return -1
 
-		def a1(test_str):
+			# finally check for unbalanced string
+			if current_max != 0:
+				return -1
+
+			return max
+
+	###############################
+	###############################
+	###############################
+	###############################
+	###############################
+	###############################
+	# function for get first variation
+	# primeste textul din prima paranteza sau a doua sau a treia incastrate una in alta, in functie de
+	# valoarea nrVar
+
+		# def a1(test_str, nrVar):
+		# 	ret = ''
+		# 	skip2c = 0
+		# 	for i in test_str:
+		# 		if i == '(':
+		# 			skip2c += 1
+		# 		elif i == ')'and skip2c > 0:
+		# 			skip2c -= 1
+		# 		elif skip2c == nrVar:
+		# 			ret += i
+		# 	return ret
+
+####################################################
+####################################################
+
+		###############################
+		# function for get first variation
+		# primeste textul din prima paranteza sau a doua sau a treia incastrate una in alta, in functie de
+		# valoarea nrVar
+
+		def a1(test_str, nrVar, nrSubVarSec):
 			ret = ''
 			skip2c = 0
+			int = 0
 			for i in test_str:
 				if i == '(':
 					skip2c += 1
-				elif i == ')'and skip2c > 0:
+					if skip2c == 1:
+						int += 1
+				elif i == '(' and skip2c == 1:
+					int += 1
+					print int
+				elif i == ')' and skip2c > 0:
 					skip2c -= 1
-				elif skip2c == 1:
+				elif skip2c == nrVar and int == nrSubVarSec:
 					ret += i
 			return ret
 
+	####################################################
 
 
-	
-		inputPng = open('text.pgn').read()
-
-		 
+		inputPng = open('d:\\programare\\pyqt\\chessTreeOpening\\text.pgn').read()
 
 		# remove head of pgn with rectangel brackets
 		finalString = re.sub(r'\[.*?\]', '', inputPng)
 
-
-		
 		# print finalString
 
 		#################################
@@ -96,9 +168,20 @@ class Window(QtGui.QMainWindow):
 
 
 		variantaPrincipala = a(finalString)
-		variantaSecundara = a1(finalString)
+		variantaSecundara = a1(finalString, 1, 2)
+		variantaTertiara = a1(finalString, 2, 1)
+		variantaCuaternara = a1(finalString, 3, 1)
+
+		for i in range(10):
+			print a1(finalString, 1, i)
 
 
+
+
+		nrParanteze = maxDepth(inputPng)
+		print "caca"
+		print 'numar paranteze'
+		print nrParanteze
 
 
 		# finalString = a1(finalString)
@@ -118,30 +201,23 @@ class Window(QtGui.QMainWindow):
 
 		###################################
 		# write header for pgn
-		with open("guru99.pgn", 'r+') as file:
-			originalContent = file.read()
-			file.seek(0, 0)              # Move the cursor to top line
-			file.write('[Event "?"]\n')
-			file.write('[Site "?"]\n')
-			file.write('[Date "????.??.??"]\n')
-			file.write('[Round "?"]\n')
-			file.write('[White "?"]\n')
-			file.write('[Black "?"]\n')
-			file.write('[Result "*"]\n')
-			file.write('[ECO "C65"]\n')
-			file.write('[Annotator "LucaCiubota_Gamer"]\n')
-			file.write('[PlyCount "31"]\n')
-			file.write('[SourceDate "2019.05.25"]\n')
-			file.write(originalContent)
-				
+		# with open("guru99.pgn", 'r+') as file:
+			# originalContent = file.read()
+			# file.seek(0, 0)              # Move the cursor to top line
+			# file.write('[Event "?"]\n')
+			# file.write('[Site "?"]\n')
+			# file.write('[Date "????.??.??"]\n')
+			# file.write('[Round "?"]\n')
+			# file.write('[White "?"]\n')
+			# file.write('[Black "?"]\n')
+			# file.write('[Result "*"]\n')
+			# file.write('[ECO "C65"]\n')
+			# file.write('[Annotator "LucaCiubota_Gamer"]\n')
+			# file.write('[PlyCount "31"]\n')
+			# file.write('[SourceDate "2019.05.25"]\n')
+			# file.write(originalContent)
 
-
-		# test new pgn
-		# f= open("guru99.pgn","w+")
-		# f.write(finalString)
-		# f.close()
-
-
+	
 
 		##################
 		# draw GUI
@@ -152,12 +228,15 @@ class Window(QtGui.QMainWindow):
 		self.textEdit = QtGui.QTextEdit()
 		self.treeWiew = QtGui.QTreeView()
 
+
 		layout = QtGui.QGridLayout()
 		layout.addWidget(self.textEdit, 0, 0)
 		layout.addWidget(self.treeWiew, 1, 0)
 
 
 		widget.setLayout(layout)
+
+
 
 	##############
 	# work on tree view
@@ -169,25 +248,34 @@ class Window(QtGui.QMainWindow):
 
 		parent1 = QtGui.QStandardItem(variantaPrincipala)
 		child1 = QtGui.QStandardItem(variantaSecundara)
-		child2 = QtGui.QStandardItem("varianta 2")
-		child3 = QtGui.QStandardItem("varianta 3 varianta 3 varianta 3 varianta 3 varianta 3 varianta 3 varianta 3 varianta 3 varianta 3 varianta 3 varianta 3 varianta 3 ")
+		child2 = QtGui.QStandardItem(variantaTertiara)
+		child3 = QtGui.QStandardItem(variantaCuaternara)
 		child4 = QtGui.QStandardItem("varianta 3 varianta 3 varianta 3 varianta 3 varianta 3 varianta 3 varianta 3 varianta 3 varianta 3 varianta 3 varianta 3 varianta 3 ")
 		child5 = QtGui.QStandardItem("varianta 3 varianta 3 varianta 3 varianta 3 varianta 3 varianta 3 varianta 3 varianta 3 varianta 3 varianta 3 varianta 3 varianta 3 ")
 		child6 = QtGui.QStandardItem("varianta 3 varianta 3 varianta 3 varianta 3 varianta 3 varianta 3 varianta 3 varianta 3 varianta 3 varianta 3 varianta 3 varianta 3 ")
-		
+
 		model.setItem(0,0, parent1)
 		parent1.setRowCount(0)
-		parent1.setColumnCount(0);
-		parent1.setChild(0, 0, child1);
-		parent1.setChild(1, 1, child2);
-		parent1.setChild(2, 2, child3);	
-		parent1.setChild(3, 3, child4);	
-		parent1.setChild(4, 4, child5);	
-		parent1.setChild(5, 5, child6);	
+		parent1.setColumnCount(0)
+		parent1.setChild(1, 1, child1)
+		parent1.setChild(2, 2, child2)
+		parent1.setChild(3, 3, child3)
+		parent1.setChild(4, 4, child4)
+		parent1.setChild(5, 5, child5)
+		parent1.setChild(6, 6, child6)
 
-		# parent1.appendColumn([child1, child2, child3])
+
+
+
+
+		self.treeWiew.expandAll()
+
+		parent1.appendColumn([child1, child2, child3])
+
+
 
 		# model.appendRow(parent1)
+
 
 
 		#set dinamicaly geometry
@@ -200,9 +288,14 @@ class Window(QtGui.QMainWindow):
 
 
 
+
+
 if __name__ == '__main__':
 	app = QtGui.QApplication([])
 	gui = Window()
 	gui.show()
 	app.exec_()
 
+#
+# chessclient = ChessClient()
+# chessclient.mainLoop()
